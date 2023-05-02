a87c9ed90af095a87da31c2848d9d1c030c30c48
HyeokminFrancis <37615624+HyeokminFrancis@users.noreply.github.com>
2023-05-02 15:38:08 +0900

    Time: 52 ms (5.22%), Space: 16.2 MB (5.10%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        c_map = {}
+        for c in s:
+            if(c in c_map):
+                c_map[c] += 1
+            else:
+                c_map[c] = 1
+        
+        max_palindrome_len = 0
+        print(c_map)
+        for c in c_map:
+            max_palindrome_len += (c_map[c] - c_map[c]%2)
+            
+        if(max_palindrome_len < len(s)):
+            max_palindrome_len += 1
+        
+        return max_palindrome_len
