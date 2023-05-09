1a5eb6643f2e3b168474601e397c89b3c9841621
HyeokminFrancis <37615624+HyeokminFrancis@users.noreply.github.com>
2023-05-09 17:27:16 +0900

    Time: 515 ms (31.56%), Space: 17.6 MB (14.34%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        maximumPoints = []
+        for i, num in enumerate(nums):
+            maximumPoints.append(i + num)
+
+        
+        
+        currIdx = 0
+        numsLen = len(nums)
+        
+        while True:
+            if maximumPoints[currIdx] >= numsLen -1:
+                return True
+            jumpRange = nums[currIdx]
+            jumpPoints = maximumPoints[currIdx + 1 : currIdx + jumpRange + 1]
+            jumpPoints = jumpPoints[::-1]
+
+            if len(jumpPoints) == 0:
+                return False
+            
+            newCurrIdx = currIdx +  len(jumpPoints) - jumpPoints.index(max(jumpPoints)) 
+            
+            
+            if(newCurrIdx <= currIdx):
+                return False
+            
+            currIdx = newCurrIdx
+            
+        
+        
+        
+        
