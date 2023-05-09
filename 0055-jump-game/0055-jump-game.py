ded9ceac8b3cd2c3adb4bfe62cc4459d3bfe50d3
HyeokminFrancis <37615624+HyeokminFrancis@users.noreply.github.com>
2023-05-09 17:28:34 +0900

    Time: 517 ms (30.38%), Space: 17.7 MB (8.16%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -7,19 +7,15 @@ class Solution:
         currIdx = 0
         numsLen = len(nums)
         while True:
-            if maximumPoints[currIdx] >= numsLen -1:
+            if maximumPoints[currIdx] >= numsLen - 1:
                 return True
             jumpRange = nums[currIdx]
             jumpPoints = maximumPoints[currIdx + 1 : currIdx + jumpRange + 1]
             jumpPoints = jumpPoints[::-1]
-
             if len(jumpPoints) == 0:
                 return False
-            
-            newCurrIdx = currIdx +  len(jumpPoints) - jumpPoints.index(max(jumpPoints)) 
-            
-            
-            currIdx = newCurrIdx
+            currIdx = currIdx +  len(jumpPoints) - jumpPoints.index(max(jumpPoints)) 
+
             
         
         
